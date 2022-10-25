adbb4a5a4373fdd272739c7a1ba99828b7542e5f
Jorge-Neves <paulo.jorge.ngs@gmail.com>
2022-10-25 23:56:00 +0100

    Create scene02 and normal mesh material cone component

--- src/scenes/Scene02.js
+++ src/scenes/Scene02.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import ConeNormal from "../components/Cones/ConeNormal";
 
 const Scene02 = () => {
   return (
-    <div>
-      <span>Scene 02</span>
-    </div>
+    <>
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="red" position={[0, 2, 5]} />
+        <ConeNormal position={[-2, 0, -5]} animation={"vertical"} />
+      </Canvas>
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="red" position={[0, 2, 5]} />
+        <ConeNormal position={[0, 0, 0]} animation={"horizontal"} />
+      </Canvas>
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="red" position={[0, 2, 5]} />
+        <ConeNormal position={[-3, -3, -3]} animation={"vertical"} />
+      </Canvas>
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="red" position={[0, 2, 5]} />
+        <ConeNormal position={[3, 2, -2]} />
+      </Canvas>
+    </>
   );
 };
 
