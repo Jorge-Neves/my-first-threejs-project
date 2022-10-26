ae32ba68c56b00bbb589612acd544ab4f72181cb
Jorge-Neves <paulo.jorge.ngs@gmail.com>
2022-10-26 01:02:41 +0100

    Create Sphere component and scenes 04 and 07

--- src/scenes/Scene04.js
+++ src/scenes/Scene04.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import SphereNormal from "../components/Spheres/SphereNormal";
 
 const Scene04 = () => {
   return (
-    <div>
-      <span>Scene 04</span>
-    </div>
+    <>
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="red" position={[0, 2, 5]} />
+        <SphereNormal position={[-2, 0, -5]} animation={"vertical"} />
+      </Canvas>
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="red" position={[0, 2, 5]} />
+        <SphereNormal position={[0, 0, 0]} animation={"horizontal"} />
+      </Canvas>
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="red" position={[0, 2, 5]} />
+        <SphereNormal position={[-3, -3, -3]} animation={"vertical"} />
+      </Canvas>
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="red" position={[0, 2, 5]} />
+        <SphereNormal position={[3, 2, -2]} />
+      </Canvas>
+    </>
   );
 };
 
--- src/scenes/Scene07.js
+++ src/scenes/Scene07.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import TriangleNormal from "../components/Triangles/TriangleNormal";
 
 const Scene07 = () => {
   return (
-    <div>
-      <span>Scene 07</span>
-    </div>
+    <>
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="red" position={[0, 2, 5]} />
+        <TriangleNormal position={[-2, 0, -5]} animation={"vertical"} />
+      </Canvas>
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="red" position={[0, 2, 5]} />
+        <TriangleNormal position={[0, 0, 0]} animation={"horizontal"} />
+      </Canvas>
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="red" position={[0, 2, 5]} />
+        <TriangleNormal position={[-3, -3, -3]} animation={"vertical"} />
+      </Canvas>
+      <Canvas>
+        <ambientLight intensity={(0, 1)} />
+        <directionalLight color="red" position={[0, 2, 5]} />
+        <TriangleNormal position={[3, 2, -2]} />
+      </Canvas>
+    </>
   );
 };
 
