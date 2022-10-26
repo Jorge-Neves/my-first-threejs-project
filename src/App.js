8e2a36e000f50b04832ba0cbfc728a5605d0a21f
Jorge-Neves <paulo.jorge.ngs@gmail.com>
2022-10-26 22:08:55 +0100

    Add Routes to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import { Routes, Route } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
 import Scene01 from "./scenes/Scene01";
 import Scene02 from "./scenes/Scene02";
 import Scene03 from "./scenes/Scene03";
@@ -32,16 +36,32 @@ function App() {
   return (
     <>
       <Navbar sceneHandler={sceneHandler} />
-      {scene === "scene-01" && <Scene01 />}
-      {scene === "scene-02" && <Scene02 />}
-      {scene === "scene-03" && <Scene03 />}
-      {scene === "scene-04" && <Scene04 />}
-      {scene === "scene-05" && <Scene05 />}
-      {scene === "scene-06" && <Scene06 />}
-      {scene === "scene-07" && <Scene07 />}
-      {scene === "scene-08" && <Scene08 />}
-      {scene === "scene-09" && <Scene09 />}
-      {scene === "scene-10" && <Scene10 />}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/scene01" element={<Scene01 />} />
+          <Route path="/scene02" element={<Scene02 />} />
+          <Route path="/scene03" element={<Scene03 />} />
+          <Route path="/scene04" element={<Scene04 />} />
+          <Route path="/scene05" element={<Scene05 />} />
+          <Route path="/scene06" element={<Scene06 />} />
+          <Route path="/scene07" element={<Scene07 />} />
+          <Route path="/scene08" element={<Scene08 />} />
+          <Route path="/scene09" element={<Scene09 />} />
+          <Route path="/scene10" element={<Scene10 />} />
+          <Route path="/scene11" element={<Scene11 />} />
+          <Route path="/scene12" element={<Scene12 />} />
+          <Route path="/scene13" element={<Scene13 />} />
+          <Route path="/scne14" element={<Scene14 />} />
+          <Route path="/scene15" element={<Scene15 />} />
+          <Route path="/scene16" element={<Scene16 />} />
+          <Route path="/scene17" element={<Scene17 />} />
+          <Route path="/scene18" element={<Scene18 />} />
+          <Route path="/scene19" element={<Scene19 />} />
+          <Route path="/scene20" element={<Scene20 />} />
+        </Routes>
+      </Router>
     </>
   );
 }
